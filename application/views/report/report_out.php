2bac470fddde91685df07398402499b2d43471fd
Baramesh Nammungkun <baramesh@icloud.com>
2017-02-25 23:55:04 +0700

    add thai

--- application/views/report/report_out.php
+++ application/views/report/report_out.php
@@ -83,10 +83,29 @@
   </div>
 
   <script type="text/javascript">
+  $.extend(true, $.fn.dataTable.defaults, {
+    "language": {
+              "sProcessing": "กำลังดำเนินการ...",
+              "sLengthMenu": "แสดง_MENU_ แถว",
+              "sZeroRecords": "ไม่พบข้อมูล",
+              "sInfo": "แสดง _START_ ถึง _END_ จาก _TOTAL_ แถว",
+              "sInfoEmpty": "แสดง 0 ถึง 0 จาก 0 แถว",
+              "sInfoFiltered": "(กรองข้อมูล _MAX_ ทุกแถว)",
+              "sInfoPostFix": "",
+              "sSearch": "ค้นหา:",
+              "sUrl": "",
+              "oPaginate": {
+                            "sFirst": "เริ่มต้น",
+                            "sPrevious": "ก่อนหน้า",
+                            "sNext": "ถัดไป",
+                            "sLast": "สุดท้าย"
+              }
+     }
+});
+
   $('.DataTable').DataTable( {
     dom: 'Bfrtip',
     buttons: [
-        'colvis',
         'excel',
         'print'
     ]
